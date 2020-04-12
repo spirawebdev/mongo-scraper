8921e98182080d9cc21d517e2c10f124511b70fb
Anthony Dees <spirawebdev@gmail.com>
2020-04-11 21:23:37 -0700

    Handlebars is fully functional

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
+// tell express to find static files in the "public" directory
+app.use(express.static('public')); 
+
+// Routes
 app.get('/', function (req, res) {
     res.render('home');
 });
  
+
+
 app.listen(3000);
